2344f425c189ecfbd0592b0316395f302e2a2168
TehSuzzles <suziepaulin@gmail.com>
2020-12-21 12:35:56 +1300

    Second sentence update.

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -3,6 +3,7 @@ public class PartD {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, My name is Suzie!");
+		System.out.println("My favourite colour is midnight blue");
 
 	}
 
