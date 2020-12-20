9055411cc937ab1b6ba543b81ddc31826443fed7
TehSuzzles <suziepaulin@gmail.com>
2020-12-21 12:39:39 +1300

    Third sentence update

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -4,7 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, My name is Suzie!");
 		System.out.println("My favourite colour is midnight blue");
-
+		System.out.println("My favourite mode of transportation is driving.");
 	}
 
 }
